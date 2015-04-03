c0f294f4f87f25fd3c84ee6f1692e82a5ea2e4dd
Andreas Wahlberg <andreas.wahlberg@live.se>
2015-04-03 10:21:39 +0200

    C#_Kod_inlagd_från_Databasen

---
+++ Kod/Vandrarhem/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Db_Vandrarhem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vandrarhem.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            using (var ctx = new VandrarhemEntities())
+            return View();
+        }
+    }
+}
